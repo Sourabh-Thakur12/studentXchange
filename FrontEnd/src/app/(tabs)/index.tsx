36b77866215398c01c06dd250f8f4db5edc9a719
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-06-17 02:41:12 +0530

    feat:sign-up ui initial setup

--- FrontEnd/src/app/(tabs)/index.tsx
+++ FrontEnd/src/app/(tabs)/index.tsx
@@ -1,15 +1,16 @@
 import "@/global.css"
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import { SignInScreen } from "@/src/components/ui/auth";
  
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       {/*for  test*/}
-      <SignInScreen />
       <Text className="text-xl font-bold text-blue-500">
         Welcome to StudentXchange!
       </Text>
+      <Link href={'/(auth)/signUp'} className="text-blue-500 bg-primary-container  m-2 px-10 py-3 rounded-3xl">Sign Up</Link>
+      <Link href={'/(auth)/signIn'} className="text-blue-500 bg-primary-container  m-2 px-10 py-3 rounded-3xl">Sign In</Link>
     </View>
   );
 }
