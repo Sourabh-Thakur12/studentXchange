139106d44cd92e4ed1a20d1b8fdbb02e0b8dc7b3
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-06-20 16:33:07 +0530

    refact:auth pulls env variables from global env instead of local env

--- BackEnd/auth/appwrite/setupdb.js
+++ BackEnd/auth/appwrite/setupdb.js
@@ -1,13 +1,12 @@
 //db schema setup
-
-require("dotenv").config();
+const config = require("../../shared/config/env")
 
 const sdk = require("node-appwrite");
 
 const client = new sdk.Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setEndpoint(config.APPWRITE_ENDPOINT)
+    .setProject(config.APPWRITE_PROJECT_ID)
+    .setKey(config.APPWRITE_API_KEY);
 
 const tablesDB = new sdk.TablesDB(client);
 
