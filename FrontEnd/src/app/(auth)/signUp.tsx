04bed67cbb820d7ae2b3f5feeea2c624cd186b56
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-06-25 12:04:19 +0530

    feat:routing to home page after regitration

--- FrontEnd/src/app/(auth)/signUp.tsx
+++ FrontEnd/src/app/(auth)/signUp.tsx
@@ -1,24 +1,40 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SignUp } from '@/src/components/ui/index'
 import { api } from '@/src/utils/apiClient'
+import { router } from 'expo-router'
 
 const SignUpPage = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  
   const onSubmit = async (name: string, email: string, password: string) => {
     try {
+      setLoading(true)
       const response = await api.post('/auth/register', { name, email, password })
       console.log(response.ok)
-      if(response.ok === false) {
+      if (!response.ok) {
+        setError(response.error)
         console.log(response.error)
+      } else {
+        router.replace("/")
+        setLoading(false)
       }
     } catch (error) {
       console.error(error)
+      setError(error instanceof Error ? error.message : String(error))
+      setLoading(false)
+    } finally {
+      setLoading(false)
     }
   };
 
   return (
     <View className='flex-1  bg-surface justify-center items-center'>
       <SignUp onSubmit={onSubmit} />
+      {loading && <Text>Loading...</Text>}
+      {error && <Text>{error}</Text>}
     </View>
   )
 }
