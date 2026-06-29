7812d722d2f847e24bac8e20941d8b5f7f9322ce
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-06-30 01:56:44 +0530

    refactor(createPost): seprated into components

---
+++ FrontEnd/src/constants/Create.ts
@@ -0,0 +1,29 @@
+export const CATEGORY_OPTIONS = [
+  {
+    label: "Books",
+    value: "books",
+  },
+  {
+    label: "Electronics",
+    value: "electronics",
+  },
+  {
+    label: "Furniture",
+    value: "furniture",
+  },
+  {
+    label: "Hostel",
+    value: "hostel",
+  },
+  {
+    label: "Sports",
+    value: "sports",
+  },
+];
+
+export const CONDITIONS = [
+  "New",
+  "Like New",
+  "Good",
+  "Fair",
+] as const;
