f2468b1c24ba325b56e3fbb1faf39da2c013ceb0
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-07-06 23:16:59 +0530

    feat: image picker added

--- FrontEnd/app.config.ts
+++ FrontEnd/app.config.ts
@@ -2,49 +2,48 @@
 // import path from 'path'
 // import { fileURLToPath } from 'url'
 
-
 // loadenv.config({
 //   path : path.resolve(fileURLToPath(import.meta.url), '../.env')
 // })
 
 export default {
-  "expo": {
-    "name": "FrontEnd",
-    "slug": "FrontEnd",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "frontend",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "splash": {
-      "image": "./assets/images/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "FrontEnd",
+    slug: "FrontEnd",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "frontend",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "ios": {
-      "supportsTablet": true
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "package": "com.sourabhthakur.FrontEnd"
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.sourabhthakur.FrontEnd",
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
-    "plugins": [
+    plugins: [
       "expo-router",
       [
         "expo-font",
         {
-          "fonts": [
+          fonts: [
             "./assets/fonts/JetBrainsMono-ExtraLight.ttf",
             "./assets/fonts/JetBrainsMono-Regular.ttf",
             "./assets/fonts/JetBrainsMono-Medium.ttf",
@@ -57,13 +56,20 @@ export default {
             "./assets/fonts/Quicksand-Regular.ttf",
             "./assets/fonts/Quicksand-Medium.ttf",
             "./assets/fonts/Quicksand-SemiBold.ttf",
-            "./assets/fonts/Quicksand-Bold.ttf"
-          ]
-        }
-      ]
+            "./assets/fonts/Quicksand-Bold.ttf",
+          ],
+        },
+      ],
+      [
+        "expo-image-picker",
+        {
+          "photosPermission":
+            "The app accesses your photos to let you share them with your friends.",
+        },
+      ],
     ],
-    "experiments": {
-      "typedRoutes": true
-    }
-  }
-}
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
