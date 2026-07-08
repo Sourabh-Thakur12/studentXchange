69d35a08a36c8655d20c630dd14c1467a24bb97b
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-07-08 23:55:03 +0530

    fixed: signup email issue in frontend

--- FrontEnd/src/app/(auth)/verifyEmail.tsx
+++ FrontEnd/src/app/(auth)/verifyEmail.tsx
@@ -22,6 +22,18 @@ export default function VerifyEmail() {
   const getParam = (value?: string | string[]) =>
     Array.isArray(value) ? value[0] : value;
 
+  // const tempURL =
+  //   "http://localhost:3000/verify-email?userId=6a4bed97002c8f500f04&secret=07b90527a5b155aaa10920724299db01f7512113e5e6fbfdee9804118cfe79a072cf24f8cfc640bf1e6cff97a489ddfa6c9a48fe09ddc77fe982dde2597637616f2e9645af6bebc750a6f54d37e85a7099c4f92826a892c6ed1ce2ecda0c84472ed816d88c06da754044f2ff80b82be61c30fab33d98df6d994d9ae4e99edd46&expire=2026-07-06T19%3A02%3A02.318%2B00%3A00";
+  // const emailAddress = tempURL.includes("email=")
+  //   ? tempURL.split("email=")[1].split("&")[0]
+  //   : null;
+  // const verificationUserId = tempURL.includes("userId=")
+  //   ? tempURL.split("userId=")[1].split("&")[0]
+  //   : null;
+  // const verificationSecret = tempURL.includes("secret=")
+  //   ? tempURL.split("secret=")[1].split("&")[0]
+  //   : null;
+
   const emailAddress = getParam(email);
   const verificationUserId = getParam(userId);
   const verificationSecret = getParam(secret);
